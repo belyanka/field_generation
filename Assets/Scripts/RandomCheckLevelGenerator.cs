8ec34cab4278bacc7928a7c53bd87bba9f1ed069
Ольга Кинчак < >
2017-11-30 14:01:50 +0300

    second generation algorithm

---
+++ Assets/Scripts/RandomCheckLevelGenerator.cs
@@ -0,0 +1,19 @@
+﻿
+public class RandomCheckLevelGenerator
+{
+    private int fieldSize;
+    private int wallBlockNumber;
+    private int enemyNumber;
+	
+    private int[][] levelGrid;
+    private int[][] stepArray;
+    
+    public void Generate(int size, int blockNumber)
+    {
+        //Initialize grid
+        
+        //Place block and check route cycle
+        
+    }
+}
+
